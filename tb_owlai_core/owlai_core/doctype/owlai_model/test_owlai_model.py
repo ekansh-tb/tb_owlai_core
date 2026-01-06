f261d362d2974cc683b640e0bb88495e2edd8c4c
ekanshjain <mailme@ekansjain.com>
2026-01-06 07:02:26 +0530

    feat: introduce OwlAI Model, Agent, Tool, Provider, and Agent Tool doctypes with their respective files.

---
+++ tb_owlai_core/owlai_core/doctype/owlai_model/test_owlai_model.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, TechBirdIt.in and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOwlAIModel(FrappeTestCase):
+	pass
---
+++ tb_owlai_core/owlai_core/doctype/owlai_provider/owlai_provider.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, TechBirdIt.in and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("OwlAI Provider", {
+// 	refresh(frm) {
+
+// 	},
+// });
